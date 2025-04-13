af6c04afc93e57c611e379484d8131d8026b0072
petro gaty <petro.gati@shulesoft.africa>
2025-04-13 11:16:39 +0300

    Refactor Docker and Nginx configurations for improved service management; update entrypoint command, expose ports, and enhance SSL settings; set base path for frontend routing.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
+const basePath = process.env.VITE_BASE_PATH || '/';
+
 export default defineConfig({
+  base: basePath,
   server: {
     host: "::",
     port: 3000,
